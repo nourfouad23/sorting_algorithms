f4fcdca139b1e3a535b6870a3421555c072c0871
Nour Fouad <Nour.Fouad@Qcentriseg.com>
2023-12-20 00:43:07 +0200

    fixes

--- sort.h
+++ sort.h
@@ -15,8 +15,8 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
 void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 #endif
